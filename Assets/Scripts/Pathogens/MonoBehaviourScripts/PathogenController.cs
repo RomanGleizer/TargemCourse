b2e5e1cdacc1dba2def732647a7524d88ca458f8
Roman <125725530+RomanGleizer@users.noreply.github.com>
2025-04-01 19:17:56 +0500

    Final, but not final, realisation of pathogens

--- Assets/Scripts/Pathogens/MonoBehaviourScripts/PathogenController.cs
+++ Assets/Scripts/Pathogens/MonoBehaviourScripts/PathogenController.cs
@@ -3,19 +3,28 @@
 public class PathogenController : MonoBehaviour
 {
     [SerializeField] private PathogenDefinition definition;
+    [SerializeField] private bool isCombatMode = false;
 
     private HealthComponent _healthComponent;
     private IPathogenBehavior _pathogenBehavior;
     private ICombatAbility _combatAbility;
     private IPathogenMovement _pathogenMovement;
-    
+
     private void Awake()
     {
         _healthComponent = GetComponent<HealthComponent>();
-        
-        _pathogenBehavior = new BasicPathogenBehavior();
-        _combatAbility = new BasicCombatAbility();
-        _pathogenMovement = new BasicPathogenMovement();
+
+        _combatAbility = new CombatAbility();
+        _pathogenMovement = new PathogenMovement();
+
+        if (isCombatMode)
+        {
+            _pathogenBehavior = new CombatPathogenBehavior(_combatAbility);
+        }
+        else
+        {
+            _pathogenBehavior = new NonCombatPathogenBehavior();
+        }
     }
 
     private void Start()
@@ -26,32 +35,25 @@ public class PathogenController : MonoBehaviour
         }
         else
         {
-            Debug.LogError("Impossible initialize pathogen health: " +
-                           $"{typeof(HealthComponent)} and {typeof(PathogenDefinition)} can't be null."
-            );
+            Debug.LogError("Невозможно инициализировать здоровье патогена: компоненты HealthComponent и PathogenDefinition не могут быть null.");
         }
 
         _pathogenBehavior.StartTurn();
     }
 
     /// <summary>
-    /// Выполнение действия патогена с учетом значения кубика
+    /// Выполнение действия патогена с учетом значения кубика.
+    /// В боевом режиме поведение обрабатывает проверку и активацию боевой способности,
+    /// в небоевом – запускает логику перемещения или иной соответствующий функционал.
     /// </summary>
     /// <param name="diceValue">Выпавшее значение кубика</param>
     public void PerformAction(int diceValue)
     {
-        if (_combatAbility.IsDiceValid(diceValue))
-        {
-            _pathogenBehavior.ExecuteAction(diceValue);
-        }
-        else
-        {
-            Debug.Log("Выпавшее значение кубика " + diceValue + " не соответствует требованиям способности.");
-        }
+        _pathogenBehavior.ExecuteAction(diceValue);
     }
 
     /// <summary>
-    /// Перемещает патоген к указанному узлу графа
+    /// Перемещает патогена к указанному узлу графа.
     /// </summary>
     /// <param name="targetNode">Целевой узел</param>
     public void MoveToNode(Node targetNode)
@@ -60,7 +62,7 @@ public class PathogenController : MonoBehaviour
     }
 
     /// <summary>
-    /// Возвращает текущий узел, где находится патоген
+    /// Возвращает текущий узел, где находится патоген.
     /// </summary>
     public Node GetCurrentNode()
     {
@@ -68,10 +70,21 @@ public class PathogenController : MonoBehaviour
     }
 
     /// <summary>
-    /// Завершает ход патогена
+    /// Завершает ход патогена.
     /// </summary>
     public void EndTurn()
     {
         _pathogenBehavior.EndTurn();
+    }
+
+    /// <summary>
+    /// Позволяет динамически переключать поведение патогена.
+    /// Например, при переходе с карты в бой можно вызвать этот метод и установить боевое поведение.
+    /// </summary>
+    /// <param name="newBehavior">Новая реализация IPathogenBehavior</param>
+    public void SetBehavior(IPathogenBehavior newBehavior)
+    {
+        _pathogenBehavior = newBehavior;
+        _pathogenBehavior.StartTurn();
     }
 }
