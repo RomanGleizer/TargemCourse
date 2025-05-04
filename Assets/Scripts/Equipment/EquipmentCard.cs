06a34d7d3cfb0643d3f9cbc18b8201eeec3ada0c
Roman <125725530+RomanGleizer@users.noreply.github.com>
2025-05-04 18:33:33 +0500

    Taking into account the infinite number of uses of an action card

--- Assets/Scripts/Equipment/EquipmentCard.cs
+++ Assets/Scripts/Equipment/EquipmentCard.cs
@@ -10,31 +10,28 @@ public class EquipmentCard : MonoBehaviour
 
     public EquipmentDefinition Definition => _definition;
     public int RemainingUses => _remainingUses;
-
     public void Initialize(EquipmentDefinition definition)
     {
         _definition = definition;
-
         ResetUses();
         UpdateConditionText();
     }
 
     void Start()
     {
-        // Если карточка создана из инспектора, сбросим состояния
-        if (_remainingUses == 0)
-            ResetUses();
         UpdateConditionText();
     }
 
     public void UpdateConditionText()
     {
-        _textCondition.text = _definition.Condition != null ? _definition.Condition.ConditionText : null ?? string.Empty;
+        _textCondition.text = _definition.Condition?.ConditionText ?? string.Empty;
     }
 
     public bool CanActivate(int diceValue)
     {
-        if (_remainingUses <= 0) return false;
+        if (_remainingUses == 0)
+            return false;
+
         var cond = _definition.Condition;
         return cond == null || cond.IsSatisfied(diceValue);
     }
@@ -43,26 +40,35 @@ public class EquipmentCard : MonoBehaviour
     {
         if (!CanActivate(dice.Value))
         {
-            if (_definition.Condition.ChangeCondition(dice.Value))
+            if (_definition.Condition != null)
             {
+                _definition.Condition.ChangeCondition(dice.Value);
                 _textCondition.text = _definition.Condition.ConditionText;
                 Destroy(dice.gameObject);
-            };
+            }
 
-            if (!CanActivate(dice.Value)) return;
+            if (!CanActivate(dice.Value))
+                return;
 
             _definition.Condition.ResetCondition();
-        };
+        }
 
         Destroy(dice.gameObject);
+
         foreach (var effect in _definition.Effects)
             effect.ApplyEffect(attacker, target, dice.Value);
 
-        _remainingUses--;
-        if (_remainingUses <= 0)
-            Destroy(gameObject);
-        else
-            UpdateConditionText();
+        if (_remainingUses > 0)
+        {
+            _remainingUses--;
+            if (_remainingUses == 0)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        UpdateConditionText();
     }
 
     public void ResetUses()
